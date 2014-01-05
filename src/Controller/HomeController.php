4e1c5a55ff67796298a0d0b3a2a695299e553eb2
Carl Markham <doddsey65@hotmail.com>
2014-01-05 17:17:06 +0000

    added email class

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,10 @@ class HomeController
 		$forums = $this->app['forum']->find_all();
 
 		\Mailer::setTemplate('emailConfirmation', array(
-			'username' => 'cjmarkham'
+			'username' => 'cjmarkham',
+			'boardTitle' => $this->app['config']->board('name'),
+			'boardUrl'   => $this->app['config']->board('url'),
+			'confirmCode' => base64_encode('doddsey65@hotmail.com-1')
 		));
 
 		\Mailer::send('doddsey65@hotmail.com', 'no-reply@asimpleforum.com', 'Email confirmation');
