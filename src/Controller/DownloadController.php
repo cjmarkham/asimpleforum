ccd004dbacc6eb6dbec801e796a150480ec5a204
Carl Markham <doddsey65@hotmail.com>
2014-01-13 10:01:23 +0000

    changed download controller to new method

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -2,15 +2,21 @@
 
 namespace Controller;
 
-class DownloadController
+class DownloadController extends Controller
 {
 	public $app;
+
+	public function __construct(\Silex\Application $app)
+	{
+		$this->app = $app;
+		$this->init();
+	}
 
 	public function index()
 	{ 
 		return $this->app['twig']->render('Download/index.twig', array(
 			'title' 			=> 'Download ASF',
 			'section'			=> 'download'
-		));
+		) + $this->extras);
 	}
 }
