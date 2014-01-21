2d5ec6b18056378a6a08ac88c485f4c5b97cb97d
Carl Markham <doddsey65@hotmail.com>
2014-01-21 15:31:05 +0000

    added trailing slash to routes

--- src/routes.php
+++ src/routes.php
@@ -8,7 +8,7 @@ $app->get('/', function (Application $app) {
     return Route::get('home:index');
 });
 
-$app->get('/test', function (Application $app) {
+$app->get('/test/', function (Application $app) {
     return include 'test.php';
 });
 
@@ -16,15 +16,15 @@ $app->get('/new-topics', function (Application $app) {
    return Route::get('topic:newest');
 });
 
-$app->get('/signup', function (Application $app) {
+$app->get('/signup/', function (Application $app) {
     return Route::get('auth:signup');
 });
 
-$app->get('/members', function (Application $app) {
+$app->get('/members/', function (Application $app) {
     return Route::get('members:index');
 });
 
-$app->get('/faqs', function (Application $app) {
+$app->get('/faqs/', function (Application $app) {
     return Route::get('faq:index');
 });
 
@@ -32,15 +32,11 @@ $app->post('/signup', function (Request $request) {
     return Route::get('auth:signup', $request);
 });
 
-$app->get('/search', function (Application $app) {
+$app->get('/search/', function (Application $app) {
     return Route::get('search:index');
 });
 
-$app->get('/search', function (Application $app) {
-    return Route::get('search:index');
-});
-
-$app->get('/search/{query}/{selection}', function (Application $app, $query, $selection) {
+$app->get('/search/{query}/{selection}/', function (Application $app, $query, $selection) {
     return Route::get('search:get', $query, $selection);
 });
 
@@ -52,23 +48,23 @@ $app->post('/login', function (Request $request) use ($app) {
     return $app['auth']->login($request);
 });
 
-$app->get('/logout', function (Application $app) {
+$app->get('/logout/', function (Application $app) {
     return Route::get('auth:logout');
 });
 
-$app->get('/download', function (Application $app) {
+$app->get('/download/', function (Application $app) {
     return Route::get('download:index');
 });
 
-$app->get('/user/{username}/{page}', function (Application $app, $username, $page) {
+$app->get('/user/{username}/{page}/', function (Application $app, $username, $page) {
     return Route::get('user:index', $username, $page);
 })->assert('page', '([0-9]+)');
 
-$app->get('/user/{username}', function (Application $app, $username) {
+$app->get('/user/{username}/', function (Application $app, $username) {
     return Route::get('user:index', $username);
 });
 
-$app->get('/user/confirmEmail/{code}', function (Application $app, $code) {
+$app->get('/user/confirmEmail/{code}/', function (Application $app, $code) {
     return Route::get('user:confirmEmail', $code);
 });
 
@@ -91,25 +87,25 @@ $app->post('/partial/{name}', function (Request $request, $name) use ($app) {
     return $app['twig']->render('Partials/' . $name . '.twig', $array);
 });
 
-$app->get('/partial/{name}', function (Application $app, $name) {
+$app->get('/partial/{name}/', function (Application $app, $name) {
     return $app['twig']->render('Partials/' . $name . '.twig', array(
         'user' => $app['session']->get('user')
     ));
 });
 
-$app->get('/{name}-{id}/{page}', function (Application $app, $name, $id, $page) {
+$app->get('/{name}-{id}/{page}/', function (Application $app, $name, $id, $page) {
     return Route::get('forum:index', $name, $id, $page);
 })->assert('page', '([0-9]+)');
 
-$app->get('/{name}-{id}', function (Application $app, $name, $id) {
+$app->get('/{name}-{id}/', function (Application $app, $name, $id) {
     return Route::get('forum:index', $name, $id);
 });
 
-$app->get('/{forum_name}/{topic_name}-{topic_id}/{page}', function (Application $app, $topic_name, $topic_id, $page) {
+$app->get('/{forum_name}/{topic_name}-{topic_id}/{page}/', function (Application $app, $topic_name, $topic_id, $page) {
     return Route::get('topic:index', $topic_name, $topic_id, $page);
 })->assert('page', '([0-9]+)');
 
-$app->get('/{forum_name}/{topic_name}-{topic_id}', function (Application $app, $topic_name, $topic_id) {
+$app->get('/{forum_name}/{topic_name}-{topic_id}/', function (Application $app, $topic_name, $topic_id) {
     return Route::get('topic:index', $topic_name, $topic_id);
 })->assert('topic_name', '([a-zA-Z0-9<>_-\s]+)');
 
@@ -159,10 +155,10 @@ $app->post('/user/{method}', function (Request $request, $method) use ($app) {
 
 // ADMIN ROUTES
 
-$app->get('/admin', function (Application $app) {
+$app->get('/admin/', function (Application $app) {
     return Route::get('admin/home:index');
 });
 
-$app->get('/admin/forums', function (Application $app) {
+$app->get('/admin/forums/', function (Application $app) {
     return Route::get('admin/forums:index');
 });
