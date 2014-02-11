0ea2130c6b5f650674bdc2400fffcd455617c276
Carl Markham <doddsey65@hotmail.com>
2014-02-11 18:43:06 +0000

    added locales folder

---
+++ src/Locales/fr.php
@@ -0,0 +1,33 @@
+<?php
+
+$translations = array(
+	'FILL_ALL_FIELDS' => 'S\'il vous plaît remplir tous les champs.',
+	'CONFIRM_TERMS' => 'Please confirm the terms and conditions.',
+	'INVALID_EMAIL' => 'Please enter a valid email address',
+	'PASSWORDS_DONT_MATCH' => 'Sorry, your entered passwords do not match.',
+	'INVALID_CREDENTIALS' => 'Les pouvoirs que vous avez saisis ne correspondent pas à nos dossiers.',
+	'USERNAME_TAKED' => 'Sorry, that username has been taken. Please choose a different one.',
+	'EMAIL_REGISTERED' => 'That email address has already been registered.',
+	 
+	'NOT_APPROVED' => 'Your email address has not been confirmed.\nYou will not be able to log in until it has been.\n\nCheck your emails for instructions on how to do so.',
+	'MUST_BE_LOGGED_IN' => 'You must be logged in to do that.',
+
+	'NO_DOUBLE_POST' => 'You cannot add another post, please edit your last post.',
+	'ALREADY_LIKED' => 'You have already liked this post.',
+
+	'TOPIC_POST_LIMIT' => 'You cannot create a new topic in this forum yet. Please wait %d minutes and %d seconds.',
+	'COMMENT_POST_LIMIT' => 'You cannot add another comment to this profile yet. Please wait %d minutes and %d seconds.',
+	'COMMENT_MIN_LENGTH' => 'Comments must be at least %d characters long.',
+	'TOPIC_LOCKED' => 'This topic is locked and you do not have permission to post in it.',
+	
+	'UNKNOWN_ERROR' => 'Something unexpected happened. Please try again later.',
+
+	'FILE_TOO_BIG' => 'The file %s is too big.\n\nThe max file size is %s kb',
+	'INVALID_FILE_EXT' => 'The file %s has an invalid extension.\n\nAllowed extensions are %s.',
+	'CANNOT_BE_BLANK' => 'Ce champ ne peut pas être laissé en blanc.',
+
+	'ACCOUNT_CREATED_CONFIRM_EMAIL' => 'Your account has been created but you will need to confirm your email address before logging in. Check your emails for details on how to do so.',
+	'ACCOUNT_CREATED' => 'Your account has been created and you can now log in.'
+);
+
+return $translations;
