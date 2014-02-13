cfac3227a85a7db1aaec814b1425ac8be46c0047
Carl Markham <doddsey65@hotmail.com>
2014-02-13 05:48:32 +0000

    updated ckeditor config

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -18,7 +18,6 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'tools' },
 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'others' },
-		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
